28e6e1c161610900f4b111edcd1024545f60198d
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-06-05 09:03:35 +0300

    Create an input and count the quotes

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -4,18 +4,25 @@ let firstQuotes = [  // Create the first quote
     "Life is what happens when you're busy making other plans",
     "Get busy living or get busy dying"
 ];
-let secondLifeQuotes = [  // This is the second quote
+let secondQuotes = [  // This is the second quote
     "You only live once, but if you do it right, once is enough",
     "Be who you are and say what you feel, because those who mind don't matter",
     " Get busy living or get busy dying"
 ];
 
-let thirdLifeQuotes = [  // Third quote
+let thirdQuotes = [  // Third quote
     "and those who matter don't mind",
     "If you work harder, your life would be succced",
     "Try to work as much as you can"
 ];
-//     let combinationAllQuotes = `${firstLifeQuotes} ${secondLifeQuotes} ${thirdLifeQuotes} ${fourthLifeQuotes} ${fourthLifeQuotes[i]}`; // Create a new variable to combine all of the quotation together
 
+let userQuotes = prompt("Choose what quotes number do you want : ")
 
-console.log(firstQuotes);
+let randomFirstQuotes = Math.floor(Math.random() * (firstQuotes.length));
+let randomSecondQuotes = Math.floor(Math.random() * (secondQuotes.length));
+let randomThirdQuotes = Math.floor(Math.random() * (thirdQuotes.length));
+
+let quotes = `${randomFirstQuotes} ${randomSecondQuotes} ${randomThirdQuotes}.`;
+
+console.log(quotes)
+
