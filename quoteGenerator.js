a5fc56a08c5292b8c73da16b6beb28df69ceeaa2
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-06-08 12:04:42 +0300

    Try to manage the exercise so that the user can access the number they want

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -23,20 +23,42 @@ let randomThirdQuotes = Math.floor(Math.random() * (thirdQuotes.length));
 let quotes = `${firstQuotes[randomFirstQuotes]} ${secondQuotes[randomSecondQuotes]} ${thirdQuotes[randomThirdQuotes]}.`;
 console.log(quotes);
 
-let random = prompt("Enter your quote number between 1 to 5: ")
 
-for (let i = 1; i <= 5; i++) {
+//1. use prompt to ask the user how many times we want to generate a random quote
+
+let random = prompt("How many quotes do you want to loop between 1 to 5: ");
+
+//2. check if the prompt give us something between 1 and 5
+
+if (random >= 1 && random <= 5) {
+    console.log(quotes)
+}
+
+const generatorQuotes = () => {
     if (random >= 1 && random <= 5) {
-        console.log(quotes);
+        console.log(quotes)
     } else {
-        console.log("invalid number");
+    console.log(quotes !== quotes)
     }
 }
 
-//1. use prompt to ask the user how many times we want to generate a random quote
-//2. check if the prompt give us something between 1 and 5
+for (let i = 1; i <= random; i++) {
+    let randomFirstQuotes = Math.floor(Math.random() * (firstQuotes.length));
+    let randomSecondQuotes = Math.floor(Math.random() * (secondQuotes.length));
+    let randomThirdQuotes = Math.floor(Math.random() * (thirdQuotes.length));
+
+    let array1 = firstQuotes[randomFirstQuotes];
+    let array2 = secondQuotes[randomSecondQuotes];
+    let array3 = thirdQuotes[randomThirdQuotes];
+
+    let quotes = `${array1} ${array2} ${array3}.`;
+    console.log(quotes);
+
+    console.log(generatorQuotes());
+}
+
 //3. we're going to use a loop somewhere, and it's going to last the number of times that the user entered in the prompt!
-//4. refactor our code into a function, and put the function call inside the loop
+//4. refactor our code into a function, and put the function call inside the loop 
 //4.1 generate random numbers
 //4.2 show the generated quotes
 
