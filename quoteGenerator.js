55919d350a97dd9c0c9ef2c2a89f97b073980cb6
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-06-08 08:29:08 +0300

    Trying to use the for loop

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -16,8 +16,6 @@ let thirdQuotes = [  // Third quote
     "Try to work as much as you can"
 ];
 
-let random = prompt("Enter your quote number: ")
-
 let randomFirstQuotes = Math.floor(Math.random() * (firstQuotes.length));
 let randomSecondQuotes = Math.floor(Math.random() * (secondQuotes.length));
 let randomThirdQuotes = Math.floor(Math.random() * (thirdQuotes.length));
@@ -25,10 +23,20 @@ let randomThirdQuotes = Math.floor(Math.random() * (thirdQuotes.length));
 let quotes = `${firstQuotes[randomFirstQuotes]} ${secondQuotes[randomSecondQuotes]} ${thirdQuotes[randomThirdQuotes]}.`;
 console.log(quotes);
 
+let random = prompt("Enter your quote number between 1 to 5: ")
 
-for (let i = 1; i < 5; i++) {
-    if (random === 1) {
-        console.log(array.push(quotes[i]));
+for (let i = 1; i <= 5; i++) {
+    if (random >= 1 && random <= 5) {
+        console.log(quotes);
+    } else {
+        console.log("invalid number");
     }
-    console.log(quotes)
 }
+
+//1. use prompt to ask the user how many times we want to generate a random quote
+//2. check if the prompt give us something between 1 and 5
+//3. we're going to use a loop somewhere, and it's going to last the number of times that the user entered in the prompt!
+//4. refactor our code into a function, and put the function call inside the loop
+//4.1 generate random numbers
+//4.2 show the generated quotes
+
