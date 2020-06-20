d70600122f9d03f570faf6801c433a1e2cc6c2b2
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-06-20 15:37:50 +0300

    Working on the quote generator using while loop

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,4 +1,4 @@
-let HappyArray = [
+let happyArray = [
     [
         "The purpose of our lives is to be happy",
         "Life is what happens when you're busy making other plans",
@@ -20,8 +20,8 @@ let HappyArray = [
 let sadArray = [
     [
         "I choose a lazy person to do a hard job, because a lazy person will find an easy way to do it",
-        "No man goes before his time—unless the boss leaves early",
-        "Doing nothing is very hard to do… you never know when you’re finished"
+        "No man goes before his time unless the boss leaves early",
+        "Doing nothing is very hard to do you never know when you’re finished"
     ],
     [
         "It takes less time to do a thing right, than it does to explain why you did it wrong",
@@ -33,4 +33,47 @@ let sadArray = [
         "People who never do any more than they get paid for, never get paid for any more than they do",
         "The best way to appreciate your job is to imagine yourself without one"
     ]
-];
+];  
+
+// create a function to random the quotes
+
+const generateQuote = (quotesArray) => {
+    let randomIndex1 = Math.floor(Math.random() * quotesArray[0].length);
+    let randomIndex2 = Math.floor(Math.random() * quotesArray[1].length);
+    let randomIndex3 = Math.floor(Math.random() * quotesArray[2].length);
+    let stringQuotes1 = quotesArray[0][randomIndex1];
+    let stringQuotes2 = quotesArray[1][randomIndex2];
+    let stringQuotes3 = quotesArray[2][randomIndex3];
+    const quoteGenerator = `Generated quote : "${stringQuotes1} ${stringQuotes2} ${stringQuotes3}"`;
+    return quoteGenerator;
+};
+
+
+let continueProgram = 'yes';
+while (continueProgram === 'yes') {
+
+    let numberOfQuotes = Number(prompt('How many quotes do you want to generate? (1min, 5max)'));
+
+    while (numberOfQuotes < 0 || numberOfQuotes > 5) { // Looping untill the users enter a right number
+        numberOfQuotes = Number(prompt('How many quotes do you want to generate? (1min, 5max)'));
+    }
+
+    let typeOfQuote = prompt('Which set of quotes do you want to use? (happy or sad)');
+
+    while (typeOfQuote !== 'happy' && typeOfQuote !== 'sad') { // Continue asking when the user enter a wrong choice
+        typeOfQuote = prompt('Which set of quotes do you want to use? (happy or sad)');
+    }
+
+    if (typeOfQuote === 'happy') { // When choosing a happy quotes
+        for (let i = 0; i < numberOfQuotes; i++) {
+            alert(`\n ${generateQuote(sadArray)} \n`);
+        }
+    } else { // Choosing sad quotes
+        for (let i = 0; i < numberOfQuotes; i++) {
+            alert(`\n ${generateQuote(sadArray)} \n`);
+        }
+    }
+    // Keep asking if enter nothing
+    continueProgram = prompt('Do you want to run the program again? (yes, no)');
+}
+alert('Thank you for using my quote generator');
